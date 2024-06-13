eb09cdaae77f507ad2f0a16cbe15345e4c682d7a
yuuyu00 <my219813@gmail.com>
2024-06-13 18:37:22 +0900

    fix build command

--- packages/frontend/src/screens/Home.tsx
+++ packages/frontend/src/screens/Home.tsx
@@ -4,15 +4,6 @@ export const Home = () => {
       <a className="underline hover:text-slate-400" href="/articles">
         Articles
       </a>
-      <a className="underline hover:text-slate-400" href="/articles/1">
-        Articles 1
-      </a>
-      <a className="underline hover:text-slate-400" href="/articles/2">
-        Articles 2
-      </a>
-      <a className="underline hover:text-slate-400" href="/articles/3">
-        Articles 3
-      </a>
     </div>
   );
 };
